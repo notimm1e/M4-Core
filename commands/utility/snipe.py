119c72079ac0f0b8b6389f604ef9db51c005d412
nyxgoober <nyxgoober@duck.com>
2026-04-07 21:48:12 +0700

    Prevented confessions from being deanonymized

--- commands/utility/snipe.py
+++ commands/utility/snipe.py
@@ -11,30 +11,51 @@ class snipe(commands.Cog):
     async def on_message_delete(self, message):
         if message.author.bot:
             return
-        self.sniped[message.channel.id] = {
-            "content": message.content,
-            "author": message.author,
-            "avatar": message.author.display_avatar.url,
-            "at": datetime.utcnow()
-        }
 
-    @commands.command(name="snipe", aliases=["s"])
+        # Check if the message is a confession
+        # We check for "!confess " to ensure it's the actual command
+        is_confession = message.content.lower().startswith("!confess ")
+
+        if is_confession:
+            # Redact information for confessions
+            self.sniped[message.channel.id] = {
+                "content": message.content,
+                "author_name": "[REDACTED]",
+                "avatar": "https://i.imgur.com/839I7sH.png", # Transparent/Empty placeholder
+                "at": datetime.utcnow(),
+                "is_anonymous": True
+            }
+        else:
+            # Store normal message data
+            self.sniped[message.channel.id] = {
+                "content": message.content,
+                "author_name": message.author.display_name.lower(),
+                "avatar": message.author.display_avatar.url,
+                "at": datetime.utcnow(),
+                "is_anonymous": False
+            }
+
+    @commands.hybrid_command(name="snipe", aliases=["s"], description="retrieve the last deleted message")
     async def snipe(self, ctx):
         data = self.sniped.get(ctx.channel.id)
+        
         if not data:
             return await ctx.send(embed=discord.Embed(
-                title="✖ nothing to snipe",
-                description="no recently deleted messages in this channel.",
-                color=discord.Color.red()
+                description="⊘ no data found in the buffer for this channel.",
+                color=0xff4500
             ))
 
         embed = discord.Embed(
-            title="⌖ sniped",
-            description=data["content"] or "*[no text content]*",
-            color=discord.Color.blue()
+            description=data["content"] or "*[no data]*",
+            color=0x2b2d31
         )
-        embed.set_author(name=data["author"].display_name, icon_url=data["avatar"])
-        embed.set_footer(text=f"deleted at {data['at'].strftime('%b %d, %Y %H:%M')} UTC")
+        
+        # Set the author based on whether it was redacted or not
+        embed.set_author(name=f"╼ {data['author_name']} ╾", icon_url=data["avatar"])
+        
+        timestamp = data['at'].strftime('%H:%M:%S')
+        embed.set_footer(text=f"⌬ captured at {timestamp} UTC")
+        
         await ctx.send(embed=embed)
 
 async def setup(bot):
