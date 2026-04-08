e4a1c1866c4c2a29c01963e2d62edb0f9123a0fd
nyx <nyxgoober@duck.com>
2026-04-08 16:28:24 +0700

    Debt + Beg

--- commands/economy/beg.py
+++ commands/economy/beg.py
@@ -1,7 +1,7 @@
 import discord
 import random
 from discord.ext import commands
-from commands.economy.economy_base import load_bank, save_bank, open_account, get_cooldown, set_cooldown
+from commands.economy.economy_base import load_bank, save_bank, open_account, get_cooldown, set_cooldown, apply_earnings, debt_prompt
 
 COOLDOWN = 120
 
@@ -15,26 +15,31 @@ class Beg(commands.Cog):
         data = open_account(ctx.author.id, data)
         user_id = str(ctx.author.id)
 
+        data = await debt_prompt(ctx, self.bot, data, ctx.author.id)
+
         remaining = get_cooldown(ctx.author.id, data, "last_beg", COOLDOWN)
         if remaining:
             return await ctx.send(embed=discord.Embed(
                 description=f"⧖ retry in {remaining}s", color=0xff4500
             ), ephemeral=True)
 
-        embed = discord.Embed(color=0xfee75c)
+        set_cooldown(ctx.author.id, data, "last_beg")
+
         if random.random() < 0.3:
-            embed.description = "⊘ request denied by network users."
-            set_cooldown(ctx.author.id, data, "last_beg")
             save_bank(data)
-            return await ctx.send(embed=embed)
+            return await ctx.send(embed=discord.Embed(
+                description="⊘ request denied by network users.", color=0xfee75c
+            ))
 
         earnings = random.randint(10, 85)
-        data[user_id]["wallet"] += earnings
-        set_cooldown(ctx.author.id, data, "last_beg")
+        debt_paid, to_wallet = apply_earnings(user_id, data, earnings)
         save_bank(data)
 
-        embed.description = f"⌬ **{earnings}** cores added to your wallet."
-        await ctx.send(embed=embed)
+        desc = f"⌬ **{earnings}** cores added to your wallet."
+        if debt_paid:
+            desc += f"\n⌬ {debt_paid:,} went toward your debt."
+
+        await ctx.send(embed=discord.Embed(description=desc, color=0xfee75c))
 
 async def setup(bot):
     await bot.add_cog(Beg(bot))
